c3ff0b48de8bc8cad56ee40af4ae0f9c9da86e7f
narendra9582 <46630244+narendra9582@users.noreply.github.com>
2020-04-17 13:18:56 +0530

    Add files via upload

---
+++ src/test/java/DemoTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class DemoTest {
+@Test
+public void simpleTest()
+{
+	int a=1;
+	int b=2;
+	assertTrue( a + b == 3);
+}
+
+@Test
+public void simpleTest1()
+{
+	int a=1;
+	int b=1;
+	assertTrue( a + b == 2);
+}
+
+}
